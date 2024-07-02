5e4b191362a0b71102c10ccc31ff30187b1d204d
MarcoTamb <marcotamburro96@gmail.com>
2024-07-02 09:20:59 +0200

    Increased to 10 years (to better show the increase)

--- utils/constants.py
+++ utils/constants.py
@@ -5,7 +5,7 @@ PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 APP_TITLE="Bradyseism Monitor"
 GITHUB_URL= 'https://github.com/MarcoTamb/Phlegrean-Fields'
 
-MIN_DATE=timedelta(days=730)
+MIN_DATE=timedelta(days=3650)#(days=730)
 
 LATITUDE='40.8270'
 LONGITUDE='14.1420'
